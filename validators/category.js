ba615967338b27bdec9febce9b81d480d25ec45f
saif hany <saifhany898@gmail.com>
2022-01-26 15:13:53 +0200

    :necktie: add "validators/category.js" add  categoryCreateValidator validator

---
+++ validators/category.js
@@ -0,0 +1,10 @@
+const { check } = require('express-validator');
+
+exports.categoryCreateValidator = [
+    check('name')
+    .notEmpty()
+    .withMessage('Name is required')
+    .isString()
+    .withMessage('Name must be a string')
+    
+]
